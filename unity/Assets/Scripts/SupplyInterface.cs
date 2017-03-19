65fb6f3392a0745d76c4ed2793ae505fd35bae05
Vincent Kenbeek <vkenbeek@gmail.com>
2017-03-19 09:04:03 +0100

    fix

--- unity/Assets/Scripts/SupplyInterface.cs
+++ unity/Assets/Scripts/SupplyInterface.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using Newtonsoft.Json;
+//using Newtonsoft.Json;
 
 namespace AssemblyCSharp
 {
@@ -45,20 +45,7 @@ namespace AssemblyCSharp
 
 		public SupplyInterface (float maxX, float maxZ, string mapSupplies, bool random)
 		{
-			mapSupplies = "{\"robot\":{\"x\":576,\"r\":25,\"y\":417},\"points\":[{\"x\":32,\"collected\":false,\"r\":5,\"y\":545,\"score\":-1},{\"x\":22,\"collected\":false,\"r\":5,\"y\":651,\"score\":-1},{\"x\":711,\"collected\":false,\"r\":5,\"y\":873,\"score\":-1},{\"x\":750,\"collected\":false,\"r\":5,\"y\":664,\"score\":-1},{\"x\":722,\"collected\":false,\"r\":5,\"y\":692,\"score\":-1},{\"x\":812,\"collected\":false,\"r\":5,\"y\":133,\"score\":1},{\"x\":136,\"collected\":false,\"r\":5,\"y\":736,\"score\":1},{\"x\":824,\"collected\":false,\"r\":5,\"y\":511,\"score\":1},{\"x\":1170,\"collected\":false,\"r\":5,\"y\":916,\"score\":1},{\"x\":933,\"collected\":false,\"r\":5,\"y\":110,\"score\":1},{\"x\":368,\"collected\":false,\"r\":5,\"y\":596,\"score\":1},{\"x\":226,\"collected\":false,\"r\":5,\"y\":53,\"score\":1},{\"x\":388,\"collected\":false,\"r\":5,\"y\":398,\"score\":1},{\"x\":269,\"collected\":false,\"r\":5,\"y\":414,\"score\":1},{\"x\":1240,\"collected\":false,\"r\":5,\"y\":33,\"score\":1},{\"x\":1048,\"collected\":false,\"r\":5,\"y\":13,\"score\":1},{\"x\":224,\"collected\":false,\"r\":5,\"y\":230,\"score\":1},{\"x\":176,\"collected\":false,\"r\":5,\"y\":394,\"score\":1},{\"x\":877,\"collected\":false,\"r\":5,\"y\":573,\"score\":1},{\"x\":480,\"collected\":false,\"r\":5,\"y\":675,\"score\":1},{\"x\":168,\"collected\":false,\"r\":5,\"y\":827,\"score\":1},{\"x\":881,\"collected\":false,\"r\":5,\"y\":825,\"score\":1},{\"x\":791,\"collected\":false,\"r\":5,\"y\":161,\"score\":1},{\"x\":121,\"collected\":false,\"r\":5,\"y\":857,\"score\":1},{\"x\":193,\"collected\":false,\"r\":5,\"y\":641,\"score\":1},{\"x\":24,\"collected\":false,\"r\":5,\"y\":490,\"score\":1},{\"x\":307,\"collected\":false,\"r\":5,\"y\":136,\"score\":1},{\"x\":427,\"collected\":false,\"r\":5,\"y\":337,\"score\":1},{\"x\":927,\"collected\":false,\"r\":5,\"y\":823,\"score\":1},{\"x\":743,\"collected\":false,\"r\":5,\"y\":120,\"score\":1},{\"x\":870,\"collected\":false,\"r\":5,\"y\":935,\"score\":1},{\"x\":903,\"collected\":false,\"r\":5,\"y\":16,\"score\":1},{\"x\":710,\"collected\":false,\"r\":5,\"y\":592,\"score\":1},{\"x\":375,\"collected\":false,\"r\":5,\"y\":255,\"score\":1},{\"x\":580,\"collected\":false,\"r\":5,\"y\":305,\"score\":1},{\"x\":757,\"collected\":false,\"r\":5,\"y\":930,\"score\":1},{\"x\":1035,\"collected\":false,\"r\":5,\"y\":415,\"score\":1},{\"x\":609,\"collected\":false,\"r\":5,\"y\":881,\"score\":1},{\"x\":523,\"collected\":false,\"r\":5,\"y\":775,\"score\":1},{\"x\":147,\"collected\":false,\"r\":5,\"y\":756,\"score\":1},{\"x\":495,\"collected\":false,\"r\":5,\"y\":644,\"score\":1},{\"x\":455,\"collected\":false,\"r\":5,\"y\":723,\"score\":1},{\"x\":142,\"collected\":false,\"r\":5,\"y\":742,\"score\":1},{\"x\":179,\"collected\":false,\"r\":5,\"y\":117,\"score\":1},{\"x\":884,\"collected\":false,\"r\":5,\"y\":762,\"score\":1},{\"x\":479,\"collected\":false,\"r\":5,\"y\":524,\"score\":1},{\"x\":519,\"collected\":false,\"r\":5,\"y\":359,\"score\":1},{\"x\":298,\"collected\":false,\"r\":5,\"y\":108,\"score\":1},{\"x\":836,\"collected\":false,\"r\":5,\"y\":182,\"score\":1},{\"x\":440,\"collected\":false,\"r\":5,\"y\":476,\"score\":1}],\"world\":{\"x_max\":1280,\"y_max\":960}}";
-			if (!random) {
-				JsonSerializer serializer = new JsonSerializer ();
-				JsonData JSON = JsonConvert.DeserializeObject<JsonData>(mapSupplies);
-
-				robot_x = JSON.robot.x;
-				robot_z = JSON.robot.y;
-				world_max_x = JSON.world.x_max;
-				world_max_z = JSON.world.y_max;
-				foreach (IPoint point in JSON.points) {
-					Point newPoint = new Point (point.x, 0, point.y, point.score, point.collected);
-					points.Add (newPoint);
-				}
-			} else {
+			
 				robot_x = 305;
 				robot_z = 212;
 				System.Random rand = new System.Random ();
@@ -74,7 +61,7 @@ namespace AssemblyCSharp
 					Point point = new Point (x, 0, z, score, false);
 					this.points.Add (point);
 				}
-			}
+
 		}
 
 		/*public SupplyInterface (float maxX, float maxZ, string mapSupplies)
