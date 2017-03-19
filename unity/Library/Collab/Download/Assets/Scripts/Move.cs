913adfcff4a4602ea209825488d29238df16b314
Liza de Graaf <liza.de.graaf@hva.nl>
2017-03-19 08:44:32 +0100

    Added Rocket

--- unity/Library/Collab/Download/Assets/Scripts/Move.cs
+++ unity/Library/Collab/Download/Assets/Scripts/Move.cs
@@ -2,20 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Move : MonoBehaviour {
     private Robot robot;
     public OVRInput.Controller Controller;
     public GameObject player;
-    
+    public string url = "http://25.92.54.140/8080";
     void Start () {
         this.robot = new Robot(player);
+
+        StartCoroutine(GetText());
     }
-	
-	void Update ()
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        yield return www.Send();
+
+        if (www.isError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+        }
+    }
+
+    void Update ()
     {
 
         OVRInput.Update();
+        StartCoroutine(GetText());
         if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) && OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
         {
             Debug.Log(OVRInput.GetLocalControllerPosition(Controller).x);
@@ -29,12 +49,12 @@ public class Move : MonoBehaviour {
                 player.transform.Rotate(0, 50 * Time.deltaTime, 0);
                 //SEND mqtt turn right
             }
-            else if (OVRInput.GetLocalControllerPosition(Controller).x < 0.1 && OVRInput.GetLocalControllerPosition(Controller).z == 0.0)
+            else if (OVRInput.GetLocalControllerPosition(Controller).x <= 0.2 && OVRInput.GetLocalControllerPosition(Controller).x >= 0.0 && OVRInput.GetLocalControllerPosition(Controller).z >= 0.0)
             {
                 player.transform.Rotate(0, 50 * Time.deltaTime, 0);
                 //SEND mqtt forward
             }
-            else if (OVRInput.GetLocalControllerPosition(Controller).x == 0.0 && OVRInput.GetLocalControllerPosition(Controller).z == -0.1)
+            else if (OVRInput.GetLocalControllerPosition(Controller).x <= 0.2 && OVRInput.GetLocalControllerPosition(Controller).x >= 0.0 && OVRInput.GetLocalControllerPosition(Controller).z < 0.0)
             {
                 player.transform.Rotate(0, -50 * Time.deltaTime, 0);
                 //SEND mqtt backward
